c2fff12c2bc0f8de9b01829ac839ecc862f9fa98
Adriano Chambel M. Lima <acmlima.softweb@gmail.com>
2016-06-24 19:06:49 -0300

    Add support for zip file output

--- js/upload.js
+++ js/upload.js
@@ -7,14 +7,19 @@ var lbEach = document.getElementById('lbEach');
 var lbAppendText = document.getElementById('lbAppendText');
 var toBytes = document.getElementById('toBytes');
 var convertToBytes = document.getElementById('convertToBytes');
+var zipElement = document.getElementById('zip');
 var appendText = document.getElementById('appendText');
 var splitBtn = document.getElementById('splitBtn');
+var timerElement = document.getElementById('timer');
 var downloads = document.getElementById('downloads');
 var resetBtn = document.getElementById('resetBtn');
 var resetBtnText = document.getElementById('resetBtnText');
 var file;
 var indexFile = 1;
 var $t = chrome.i18n;
+
+var startByte;
+var endByte;
 
 headerText.textContent = $t.getMessage('headerText');
 helpTitle.title = $t.getMessage('helpTitle');
@@ -55,14 +60,23 @@ function resetDownloads() {
     downloads.innerHTML = '';
     downloads.style.visibility = 'hidden';
     resetBtn.style.visibility = 'hidden';
+    timerElement.style.visibility = 'hidden';
+    chooseFile.disabled = false;
 }
 
 function splitFile() {
-    if(!file) return;
+    if(!file || toBytes.value <= 0) return;
+
+    chooseFile.disabled = true;
+    timer = setInterval(showTimer, 1000);
 
     var reader = new FileReader();
-    reader.readAsText(file);
-    var toSave = '';
+    var lastSlice = 0;
+    linesToSave = '';
+    startByte = 0;
+    endByte = (2).megaByteToBytes();
+    initialTime = new Date();
+    zip = new JSZip();
 
     reader.onloadstart = function() {
         splitBtn.disabled = true;
@@ -73,29 +87,57 @@ function splitFile() {
         setMessage(err, 'ui negative icon message', 'frown icon');
     }
 
-    reader.onload = function (event) {
-        var fileBuffer = event.target.result.split("\n");
+    reader.onloadend = function(e) {
+        if(e.target.readyState == FileReader.DONE) {
+            var lastIndex = e.target.result.lastIndexOf("\n");
+            var data = e.target.result.substr(0, lastIndex);
+            readyToSave(data);
 
-        fileBuffer.forEach(function(line, index){
-            if((toSave+appendText.value).length <= limitOfBytes()) {
-                toSave += line+"\n";
-            }
-            else {
-                saveFile(toSave);
-                toSave = line+"\n";
-            }
-
-            if((fileBuffer.length - 1) === index) {
-                saveFile(toSave);
-                indexFile = 1;
-            }
-        });
-
-        splitBtn.disabled = false;
-        setMessage($t.getMessage('readerSuccess'), 'ui positive icon message', 'smile icon');
-
+            lastSlice += data.length + 1;
+            startByte = lastSlice;
+            endByte = lastSlice +  (2).megaByteToBytes(); // read with chunk up to 2MB
+            readBytes(this);
+        }
     }
 
+    readBytes(reader);
+
+}
+
+function readyToSave(str) {
+    var limit = limitOfBytes();
+    var fileBuffer = str.split("\n");
+
+    fileBuffer.forEach(function(line) {
+        var currentLengthLine = linesToSave.length + (line+"\n").length + appendText.value.length;
+
+        if(currentLengthLine <= limit){
+            linesToSave += line+"\n";
+        }
+        else{
+            saveFile(linesToSave);
+            linesToSave = line+"\n";
+        }
+
+    });
+
+    if((endByte >= file.size) && (linesToSave.length > 0)) {
+        saveFile(linesToSave);
+        indexFile = 1;
+    }
+
+
+}
+
+function readBytes(fr){
+    var slice = file.slice(startByte, endByte);
+
+    if(slice.size === 0) {
+        if(zipElement.checked) saveZip();
+        return;
+    }
+
+    fr.readAsBinaryString(slice);
 }
 
 function setMessage(text, className, iconClassName) {
@@ -110,29 +152,51 @@ function setMessage(text, className, iconClassName) {
 
 function saveFile(data) {
     var data = data? data+appendText.value : data;
+
+    if(zipElement.checked) {
+        zip.file(`${indexFile}-${file.name}`, new Blob([data]));
+    }
+    else{
+        createDownloadLink({data: data, zip: false});
+    }
+
+    indexFile++;
+}
+
+
+function saveZip() {
+    zip.generateAsync({type:"blob", compression:"DEFLATE"}).then(function(blob){ createDownloadLink({data: blob, zip: true}) });
+}
+
+function createDownloadLink(link) {
+    var blob = link.zip? link.data : new Blob([link.data]);
+    var fileDownload = link.zip? `${file.name}.zip` : `${indexFile}-${file.name}`;
     var div = document.createElement('div');
     var fileExport = document.createElement('a');
     var icon = document.createElement('i');
-    icon.className = 'file text outline icon';
+    icon.className = link.zip? 'file archive outline large icon' : 'file text outline large icon';
     div.appendChild(icon);
-    fileExport.href = window.URL.createObjectURL(new Blob([data.trimRight()], {type: file.type}));
-    fileExport.download = `${indexFile}-${file.name}`;
-    fileExport.textContent = `${indexFile}-${file.name}`;
+    fileExport.href = window.URL.createObjectURL(blob);
+    fileExport.download = fileDownload;
+    fileExport.textContent = fileDownload;
     div.className = 'item';
     div.appendChild(fileExport);
     downloads.appendChild(div);
     downloads.style.visibility = 'visible';
+    splitBtn.disabled = false;
     resetBtn.style.visibility = 'visible';
-    indexFile++;
+    chooseFile.disabled = false;
+    setMessage($t.getMessage('readerSuccess'), 'ui positive icon message', 'smile icon');
+    clearInterval(timer);
 }
 
 function updateFileInfo() {
     var fileInfo = document.getElementById('fileInfo');
 
     if(file) {
-        var sizeInKB = `<label class="ui tag label">${(file.size.bytesToKiloByte()).toFixed(3)} KB</label>`;
-        var sizeInMB = `<label class="ui teal tag label">${(file.size.bytesToMegaByte()).toFixed(3)} MB</label>`;
-        var sizeInGB = `<label class="ui brown tag label">${(file.size.bytesToGigaByte()).toFixed(3)} GB</label>`;
+        var sizeInKB = `<label class="ui tag label">${(file.size.bytesToKiloByte()).toLocaleString()} KB</label>`;
+        var sizeInMB = `<label class="ui teal tag label">${(file.size.bytesToMegaByte()).toLocaleString()} MB</label>`;
+        var sizeInGB = `<label class="ui brown tag label">${(file.size.bytesToGigaByte()).toLocaleString()} GB</label>`;
         fileInfo.innerHTML = `<label>${$t.getMessage('fileInfo')}</label> ${file.name} ${sizeInKB} ${sizeInMB} ${sizeInGB}`;
     }
     else {
@@ -141,10 +205,39 @@ function updateFileInfo() {
 }
 
 function updateSplitBtn() {
-    splitBtn.style.visibility = file? 'visible' : 'hidden';
+    if(file && toBytes.value > 0) {
+        splitBtn.style.visibility = 'visible';
+    }
+    else {
+        splitBtn.style.visibility = 'hidden';
+    }
 }
 
 function limitOfBytes() {
     var fn = window['Number']['prototype'][convertToBytes.value];
     if (typeof fn === 'function') return fn.call(parseInt(toBytes.value));
+}
+
+function timeElapsed(initialTime, finalTime) {
+    var ms = finalTime - initialTime;
+    var secs = ms / 1000;
+
+    var minutes = secs / 60 ; secs = secs % 60;
+    var hours   = minutes / 60 ; minutes = minutes % 60;
+
+    return {
+        hours: Math.floor(hours),
+        minutes: Math.floor(minutes),
+        secs: Math.floor(secs),
+        ms: Math.floor(ms),
+        initialTime: initialTime,
+        finalTime: finalTime
+    }
+
+}
+
+function showTimer() {
+    var time = timeElapsed(initialTime, new Date());
+    timerElement.style.visibility = 'visible';
+    document.getElementById('times').textContent = `${time.hours}:${time.minutes}:${time.secs}`;
 }
